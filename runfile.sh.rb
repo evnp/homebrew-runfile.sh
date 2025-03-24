2e8503412544130eaeb3198b8250ff3c39936d3a
Evan Purcer <ep@e2.gg>
2025-03-24 10:21:45 -0700

    add checksum

--- runfile.sh.rb
+++ runfile.sh.rb
@@ -2,6 +2,7 @@ class RunfileSh < Formula
   desc "Language-agnostic project task runner built on the ubiquitous Make."
   homepage "https://github.com/evnp/runfile.sh"
   url "https://github.com/evnp/runfile.sh/archive/v0.0.3.tar.gz"
+  sha "f3d6e37ab81318c8698a0b81b95a2b4e82f485c94ad2639361ff21710021ab06"
   license "MIT"
 
   def install
